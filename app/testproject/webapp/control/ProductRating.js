eee6e5236a4522e3cc4dead62eb1f61a974e18a7
Christian Mari Roque <christian.mari.roque@accenture.com>
2024-02-07 00:54:22 +0000

    ProjectRating JS added

---
+++ app/testproject/webapp/control/ProductRating.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+	"sap/ui/core/Control"
+], (Control) => {
+	"use strict";
+
+	return Control.extend("ui5.walkthrough.control.ProductRating", {
+		metadata : {},
+
+		init() {},
+
+		renderer(oRM, oControl) {}
+	});
+});
